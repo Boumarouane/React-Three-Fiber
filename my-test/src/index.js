f5db69b81ae486d14352cb6f5b9a27096c3405c9
Boumarouane <i.boumarouane@gmail>
2020-04-12 23:45:49 +0200

    blop

---
+++ my-test/src/index.js
@@ -0,0 +1,32 @@
+import React, { useRef } from 'react';
+import ReactDOM from 'react-dom';
+import { Canvas, useFrame } from "react-three-fiber";
+import './index.css';
+
+function Box() {
+  const mesh = useRef()
+
+  useFrame(() =>{
+    mesh.current.rotation.y += 0.01
+  })
+
+  return (
+    <mesh ref={mesh}>
+      <boxBufferGeometry attach="geometry" args={[2, 2, 2]} />
+      <meshBasicMaterial attach="material" color={0xf95b3c} />
+    </mesh>
+  );
+}
+
+function App(){
+  return(
+    <>
+      <Canvas>
+        <Box />
+      </Canvas>
+    </>
+  )
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
