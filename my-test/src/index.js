1a20e05635cc41a620899e594e10573ae94e4236
Boumarouane <i.boumarouane@gmail>
2020-04-25 01:19:11 +0200

    ajout du changement de couleur en hover

--- my-test/src/index.js
+++ my-test/src/index.js
@@ -1,24 +1,41 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState, useCallback } from 'react';
 import ReactDOM from 'react-dom';
 import { Canvas, useFrame } from "react-three-fiber";
 import './index.css';
 
-function Box() {
+// composant = cube
+const Box = () => {
+  const [isHovered, setIsHovered] = useState(false);
+  const color = isHovered ? 0xe5d54d : 0xf95b3c;
+
+  const onHover = useCallback((e, value) => {
+    e.stopPropagation();
+    setIsHovered(value);
+  }, [setIsHovered]);
+
+  // construction de notre objet
   const mesh = useRef()
 
+  // boucle d'animation
   useFrame(() =>{
     mesh.current.rotation.y += 0.01
-  })
+  });
 
   return (
-    <mesh ref={mesh}>
+    // construction de notre cube
+    <mesh 
+    ref={mesh}
+    position={[0, 2, 0]}
+    onPointerOver={e => onHover(e, true)}
+    onPointerOut={e => onHover(e, false)}
+    >
       <boxBufferGeometry attach="geometry" args={[2, 2, 2]} />
-      <meshBasicMaterial attach="material" color={0xf95b3c} />
+      <meshBasicMaterial attach="material" color={color} />
     </mesh>
   );
 }
 
-function App(){
+const App = () => {
   return (
       <Canvas>
         <Box />
